0d54b1995793bcbc9c20ab555277fee9de1650dd
JoseMenta <jrmenta2@gmail.com>
2022-05-18 20:29:18 -0300

    Agrego el arreglo de punteros a funcion

--- Kernel/irqDispatcher.c
+++ Kernel/irqDispatcher.c
@@ -4,26 +4,13 @@
 #include <naiveConsole.h>
 static void int_20();
 static void int_21();
-//typedef void (* handler) ();
-//
-//handler funcArray[2] = {int_20, int_21};              // Arreglo de punteros a funcion de interrupciones
-//void (*handlers[2])(void)  = {int_20,int_21};
-// Es la funcion que recibira el codigo de la interrupcion y, con ello, llamar a su rutina de atencion
+typedef void (* handler) ();
+handler handlers[2] = {int_20, int_21};              // Arreglo de punteros a funcion de interrupciones
+
 void irqDispatcher(uint64_t irq) {
 	//segun el parametro que tiro el IrqHandler cae en algun case
-	//eso te lleva a la funcion posta donde defino que hace la irq
-//    handler func = funcArray[irq];
-//    func();
-    //handlers[irq]();
-    switch (irq) {
-		case 0:
-			int_20();
-			break;
-        case 1:
-            int_21();
-            break;
-	}
-
+    //eso te lleva a la funcion posta donde defino que hace la irq
+    handlers[irq]();
 	return;
 }
 
