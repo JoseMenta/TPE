98342dfb6079466c6aef7a06999ef8ff095869fa
gfrancois <gfrancois@itba.edu.ar>
2022-05-26 02:49:31 -0300

    actualizar analizeBuffer

--- Userland/SampleCodeModule/programs/bash.c
+++ Userland/SampleCodeModule/programs/bash.c
@@ -114,6 +114,100 @@
 //    }
 //}
 //
+
+//void analyze_buffer_actualizado(void) {
+//    // Obtenemos los strings sin espacios de los ingresado en la terminal
+//    char * tokens = strtok(buffer, " ");
+//    // Si no se ingreso texto, solo ENTER, no se hace nada
+//    if(tokens == NULL){
+//        return;
+//    }
+//    // Si se escribe "logout", bash se cierra y se apaga la compu
+//    if(strcmp(tokens, LOGOUT) == 0){
+//        tokens = strtok(NULL, tokens);
+//        // Verificamos que solo se halla ingresado "logout" y nada mas
+//        if(tokens == NULL){
+//            sys_exit(0);
+//        }
+//        else {
+//            print_string("error: expresion invalida", RED);
+//            return;
+//        }
+//    }
+//
+//    // El primer string debe ser un programa valido
+//    void* program_a =  get_program(tokens);
+//    // Si no se encontro programa, entonces no es un string valido
+//    if(program_a == NULL){
+//        // Lanzar error: print rojo
+//        print_string("error: programa invalido", RED);
+//        return;
+//    }
+//    if(program_a == get_program("printmem")){
+//      tokens = strtok(0, tokens);
+//      if(tokens == NULL){
+//          print_string("error: Ausencia de parametros en llamada a printmem", RED);
+//          return;
+//      }else{
+//          char * param = tokens;                  //subirlo a la estruc del programa o pasarlo como parametro en sys_exec()
+//      }
+//    }
+//    // Para iterar sobre los tokens, se debe hacer strtok(NULL, tokens), donde NULL es una constante igual a 0
+//    tokens = strtok(0, tokens);
+//    // Si solo habia un string (y valido) entonces se ejecuta un programa en toda la pantalla
+//    if (tokens == NULL) {
+//        uint64_t program[1] = {(uint64_t)program_a};
+//        sys_exec(1, (void *)program);
+//        return;
+//    }
+//    // En cambio, si hay mas de un string, el proximo debe ser un pipe
+//    // Si no lo es, entonces es una expresion invalida
+//    if(strcmp(tokens, "|") != 0) {
+//        // Lanzar error: print rojo
+//        print_string("error: no usa | como separador de programas", RED);
+//        return;
+//    }
+//    tokens = strtok(0, tokens);
+//    // Si no hay un tercer string tambien es invalido pues debe ser 'pgm1 | pgm2'
+//    if(tokens == NULL){
+//        // Lanzar error: print rojo
+//        print_string("error: programa de consola derecha ausente", RED);
+//        return;
+//    }
+//    // Luego, el tercer string debe ser un programa valido
+//    void* program_b = get_program(tokens);
+//    // Si no lo es lanza error
+//    if (program_b == NULL) {
+//        // Lanzar error: print rojo
+//        print_string("error: programa para consola derecha invalido", RED);
+//        return;
+//    }
+//    if(program_a == get_program("printmem")){
+//      si es prinmem entonces debo sonsumir un tokens mas para guardar el parametro    
+//      tokens = strtok(0, tokens);
+//      if(tokens == NULL){
+//          print_string("error: Ausencia de parametros en llamada a printmem", RED);
+//          return;
+//      }else{
+//          char * param = tokens;                  //subirlo a la estruc del programa o pasarlo como parametro en sys_exec()
+//      }
+//    }
+//    // Y no debe haber mas strings, para asi ejecutar dos programas
+//    tokens = strtok(0, tokens);
+//    if (tokens == NULL) {
+//        void* program[2] = {program_a, program_b};
+//        sys_exec(2, (void *)program);
+//        return;
+//    }
+//    // Si los hay lanza error
+//    else{
+//        // Lanzar error: print rojo
+//        print_string("error: cantidad de programas invalida", RED);
+//        return;
+//    }
+//}
+
+
 ////---------------------------------------------------------------------------------
 //// clean_buffer: Limpia el buffer
 ////---------------------------------------------------------------------------------
